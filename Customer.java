fc1b177da714258c1778a674f6927159dee2e6b7
haikalaulia <167389445+haikalaulia@users.noreply.github.com>
2024-11-18 22:08:23 +0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,10 @@
+class Customer extends Akun {
+    String username;
+    String password;
+
+    Customer(String id, String username, String password) {
+        super(id);
+        this.username = username;
+        this.password = password;
+    }
+}
