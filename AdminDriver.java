82f98940a8dc8966a689e655a22ed02963c7c5b0
haikalaulia <167389445+haikalaulia@users.noreply.github.com>
2024-11-18 22:05:27 +0700

    Update AdminDriver.java

--- AdminDriver.java
+++ AdminDriver.java
@@ -1,79 +1,80 @@
-import java.io.*;
-import java.util.*;
+import java.io.*; 
+import java.util.*; 
 
-class AdminDriver extends Driver {
-    ArrayList<Barang> listBarang;
-    ArrayList<Transaksi> listTransaksi;
+public class AdminDriver extends Driver { 
+    ArrayList<Barang> listBarang; 
+    ArrayList<Transaksi> listTransaksi; 
 
     AdminDriver(Akun akun, ArrayList<Barang> listBarang, ArrayList<Transaksi> listTransaksi) {
-        this.akun = akun;
-        this.listBarang = listBarang;
-        this.listTransaksi = listTransaksi;
+        this.akun = akun; 
+        this.listBarang = listBarang; 
+        this.listTransaksi = listTransaksi; 
     }
 
     public void addBarang(Barang barang) {
-        listBarang.add(barang);
-        saveAllBarangToFile();
-        System.out.println("Barang added: " + barang.name);
+        listBarang.add(barang); 
+        saveAllBarangToFile(); 
+        System.out.println("Barang added: " + barang.name); 
     }
+
     public void editBarang(int index, Barang barang) {
-        if (index >= 0 && index < listBarang.size()) {
-            listBarang.set(index, barang);
-            saveAllBarangToFile();
-            System.out.println("Barang edited: " + barang.name);
+        if (index >= 0 && index < listBarang.size()) { 
+            listBarang.set(index, barang); 
+            saveAllBarangToFile(); 
+            System.out.println("Barang edited: " + barang.name); 
         } else {
-            System.out.println("Invalid index");
+            System.out.println("Invalid index"); 
         }
     }
 
     public void deleteBarang(int index) {
-        if (index >= 0 && index < listBarang.size()) {
-            Barang removed = listBarang.remove(index);
-            saveAllBarangToFile();
-            System.out.println("Barang deleted: " + removed.name);
+        if (index >= 0 && index < listBarang.size()) { 
+            Barang removed = listBarang.remove(index); 
+            saveAllBarangToFile(); 
+            System.out.println("Barang deleted: " + removed.name); 
         } else {
-            System.out.println("Invalid index");
+            System.out.println("Invalid index"); 
         }
     }
 
     public void viewTransaksi() {
-        for (int i = 0; i < listTransaksi.size(); i++) {
-            Transaksi transaksi = listTransaksi.get(i);
+        for (int i = 0; i < listTransaksi.size(); i++) { 
+            Transaksi transaksi = listTransaksi.get(i); 
             System.out.println("Index: " + i + ", Transaksi ID: " + transaksi.id + ", Customer ID: " + transaksi.akun.id);
         }
     }
 
     public void acceptTransaksi(int index) {
-        if (index >= 0 && index < listTransaksi.size()) {
-            Transaksi transaksi = listTransaksi.get(index);
-            updateStockAfterTransaksiAcceptance(transaksi);
-            listTransaksi.remove(index); // Remove the accepted transaction from pending list
-            System.out.println("Transaksi accepted for: " + transaksi.akun.id);
+        if (index >= 0 && index < listTransaksi.size()) { 
+            Transaksi transaksi = listTransaksi.get(index); 
+            updateStockAfterTransaksiAcceptance(transaksi); 
+            listTransaksi.remove(index); 
+            System.out.println("Transaksi accepted for: " + transaksi.akun.id); 
         } else {
-            System.out.println("Invalid index");
+            System.out.println("Invalid index"); 
         }
     }
 
     private void updateStockAfterTransaksiAcceptance(Transaksi transaksi) {
-        for (Barang transBarang : transaksi.barang) {
-            for (Barang barang : listBarang) {
-                if (barang.id.equals(transBarang.id)) {
-                    barang.quantity -= transBarang.quantity; // Reduce stock
-                    break;
+        for (Barang transBarang : transaksi.barang) { 
+            for (Barang barang : listBarang) { 
+                if (barang.id.equals(transBarang.id)) { 
+                    barang.quantity -= transBarang.quantity; 
+                    break; 
                 }
             }
         }
-        saveAllBarangToFile(); // Save updated stock levels to the file
+        saveAllBarangToFile(); 
     }
 
     private void saveAllBarangToFile() {
-        try (FileWriter writer = new FileWriter("allBarang.txt")) {
-            for (Barang barang : listBarang) {
-                writer.write(barang.id + "," + barang.name + "," + barang.quantity + "\n");
+        try (FileWriter writer = new FileWriter("allBarang.txt")) { 
+            for (Barang barang : listBarang) { 
+                writer.write(barang.id + "," + barang.name + "," + barang.quantity + "\n"); 
             }
-            System.out.println("All barang saved to allBarang.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("All barang saved to allBarang.txt"); 
+        } catch (IOException e) { 
+            e.printStackTrace(); 
         }
     }
 }
