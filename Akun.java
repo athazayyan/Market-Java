7010209f308f78457a9da457f8c50e00b3bba901
haikalaulia <167389445+haikalaulia@users.noreply.github.com>
2024-11-18 22:06:31 +0700

    Update Akun.java

--- Akun.java
+++ Akun.java
@@ -1,23 +1,7 @@
-class Akun {
+public class Akun {
     String id;
 
     Akun(String id) {
         this.id = id;
-    }
-}
-
-abstract class Driver {
-    Akun akun;
-}
-
-class Admin extends Akun {
-    Admin(String id) {
-        super(id);
-    }
-}
-
-class Customer extends Akun {
-    Customer(String id) {
-        super(id);
     }
 }
