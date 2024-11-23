273def05bf507d6c54e4bf09cd2612a9abe7c787
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-11-23 19:51:35 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,5 +1,10 @@
 public class Customer extends Akun {
-    Customer(String id) {
+    String username;
+    String password;
+
+    Customer(String id, String username, String password) {
         super(id);
+        this.username = username;
+        this.password = password;
     }
 }
