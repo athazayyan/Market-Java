4b9d8f473a67ac23500bc65d6c0e5a3e45331507
NShadiqah <167015399+NShadiqah@users.noreply.github.com>
2024-11-23 12:04:50 -0800

    Add files via upload

---
+++ Barang.java
@@ -0,0 +1,11 @@
+public class Barang {
+    String id;
+    String name;
+    int quantity;
+
+    Barang(String id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
