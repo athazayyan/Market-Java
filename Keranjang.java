f1b887315d70cb93d99001deb3fc1217d4e61352
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-11-24 19:14:40 +0700

    Update Keranjang.java

--- Keranjang.java
+++ Keranjang.java
@@ -3,4 +3,32 @@ import java.util.ArrayList;
 public class Keranjang {
     private ArrayList<Barang> barang;
 
+    public Keranjang() {
+        this.barang = new ArrayList<>();
+    }
+
+    public void addBarang(Barang barang) {
+        this.barang.add(barang);
+    }
+
+    public void clear() {
+        this.barang.clear();
+    }
+
+    public ArrayList<Barang> getBarang() {
+        return new ArrayList<>(this.barang);
+    }
+
+    public boolean isEmpty() {
+        return this.barang.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Barang b : barang) {
+            sb.append(b.name).append(" (x").append(b.quantity).append("), ");
+        }
+        return sb.toString();
+    }
 }
