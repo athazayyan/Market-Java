a9bcbcf72e7fcc0b1878829881ec2e96ff6d21b3
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-11-25 16:15:42 +0700

    Update Barang.java

--- Barang.java
+++ Barang.java
@@ -2,10 +2,12 @@ public class Barang {
     String id;
     String name;
     int quantity;
+    int price;
 
-    Barang(String id, String name, int quantity) {
+    Barang(String id, String name, int quantity, int price) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
+        this.price = price;
     }
 }
