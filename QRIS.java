29b4226cd7513c2d31c8de4b5c9ed662784d0a2e
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-11-26 21:11:25 +0700

    Update QRIS.java

--- QRIS.java
+++ QRIS.java
@@ -6,7 +6,7 @@ public class QRIS extends Pembayaran{
     }
 
     @Override
-    public void setJumlahBayar(String nama, int jumlahBayar, int Kuantity){
+    public void setJumlahBayar(int index, String nama, int jumlahBayar, int Kuantity){
         int jumlahBayarQRIS = jumlahBayar * Kuantity;
         System.out.println("Jumlah pembayaran QRIS berhasil diatur, jumlah yang anda Bayarkan adalah" + jumlahBayarQRIS+ "dengan nama" + nama);
     }
