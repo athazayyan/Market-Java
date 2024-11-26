3740e339c4fdcdd24ed1e731be2570ff505393e5
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-11-26 19:28:02 +0700

    Update COD.java

--- COD.java
+++ COD.java
@@ -6,8 +6,9 @@ public class COD extends Pembayaran{
     }
 
     @Override
-    public void setJumlahBayar(){
-        System.out.println("Jumlah pembayaran COD berhasil diatur");
+    public void setJumlahBayar(String nama, int jumlahBayar, int Kuantity){
+        int jumlahBayarCOD = jumlahBayar * Kuantity;
+        System.out.println("Jumlah pembayaran COD berhasil diatur, dengan jumlah yang anda bayarkan adalah Rp. " + jumlahBayarCOD + " dengan nama " + nama);
     }
     
 }
