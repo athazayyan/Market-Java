46e5fcd4569642f026a42aa498b01fa4a34b94cb
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-11-26 19:26:34 +0700

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -6,8 +6,9 @@ public class Bank extends Pembayaran{
     }
 
     @Override
-    public void setJumlahBayar(){
-        System.out.println("Jumlah pembayaran Bank berhasil diatur");
+    public void setJumlahBayar(String nama, int jumlahBayar, int Kuantity){
+        int jumlahBayarBank = jumlahBayar * Kuantity;
+        System.out.println("Jumlah pembayaran Bank berhasil diatur, dengan jumlah yang anda bayarkan adalah Rp. " + jumlahBayarBank + " dengan nama " + nama);
     }
 // Sementaraaa ini dlu
 }
