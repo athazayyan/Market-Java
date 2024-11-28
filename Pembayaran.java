331ec6da1afb48ae563466825653741a0a1392ab
haikalaulia <167389445+haikalaulia@users.noreply.github.com>
2024-11-28 10:36:40 +0700

    Update Pembayaran.java

--- Pembayaran.java
+++ Pembayaran.java
@@ -1,8 +1,9 @@
 abstract public class Pembayaran {
     String idPembayaran;
     String tanggal;
+    Invoice invoice;
 
     abstract public void bayar();
-    abstract public void setJumlahBayar();
+    abstract public void setJumlahBayar(int index, String nama, int jumlahBayar, int Kuantity);
     
 }
