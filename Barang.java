a81b58a76a3d68f8ca3482a592a61719ec167439
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-11-28 10:30:20 +0700

    Add files via upload

---
+++ Barang.java
@@ -0,0 +1,13 @@
+public class Barang {
+    String id;
+    String name;
+    int quantity;
+    int price;
+
+    Barang(String id, String name, int quantity, int price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
