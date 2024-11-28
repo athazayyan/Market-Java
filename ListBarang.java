e1c2b39a8c4106d1655f976ad705702c118ce11b
haikalaulia <167389445+haikalaulia@users.noreply.github.com>
2024-11-28 10:35:49 +0700

    Update ListBarang.java

--- ListBarang.java
+++ ListBarang.java
@@ -3,30 +3,31 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ListBarang {
+    Keranjang keranjang = new Keranjang();
 
-    // Metode untuk menampilkan daftar barang
     public void listBarang() {
         String filePath = "allBarang.txt";
-        System.out.println("+--------------------------------------------------------+");
-        System.out.println("| No | ID Barang | Nama Barang       | Stok             |");
-        System.out.println("+--------------------------------------------------------+");
+        System.out.println("+---------------------------------------------------------------------+");
+        System.out.println("| No | ID Barang | Nama Barang       | Stok  | Harga                 |");
+        System.out.println("+---------------------------------------------------------------------+");
         
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             int index = 0; 
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
-                if (data.length == 3) {
+                if (data.length == 4) {
                     String id = data[0].trim();
                     String name = data[1].trim();
                     String stock = data[2].trim();
-                    System.out.printf("| %-2d | %-9s | %-16s | %-15s |\n", index++, id, name, stock);
+                    String price = data[3].trim();
+                    System.out.printf("| %-2d | %-9s | %-16s | %-5s | %-20s |\n", index++, id, name, stock, price);
                 }
             }
         } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
+            System.err.println("Error ketika baca file: " + e.getMessage());
         }
 
-        System.out.println("+--------------------------------------------------------+");
+        System.out.println("+---------------------------------------------------------------------+");
     }
 }
